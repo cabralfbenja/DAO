71a541381245048a442b48a6abf63d5f31460100
alumno203 alumno203 <alumno203@labsis-a2m3.labsis.frc.utn.edu.ar>
2023-08-23 19:34:16 -0300

    personas.py mejorado

--- Archivos/Personas/personas.py
+++ Archivos/Personas/personas.py
@@ -5,17 +5,11 @@ def ingresarApellido():
 def main():
     apellido = input("Ingrese el apellido a buscar: ")
     archivo = open("personas.txt", "r")
-    nombre, edad = [], []
+    count = 0
     for linea in archivo.readlines():
         datos = linea.strip().split(",")
         if datos[2] == apellido.upper():
-            nombre.append(datos[1])
-            edad.append(datos[3])
+            count += 1
+            print(count, "| Nombre:", datos[1], "| Edad:", datos[3])
     
-    if len(nombre)>0:
-        for i in range(len(nombre)):
-            print("Nombre:", nombre[i])
-            print("Edad:", edad[i])
-    else:
-        print("No se encontro...")
 main()
