398b72265780c4187d7c7b7737d7b31818dcf0d0
alumno702 alumno702 <alumno702@labsis-a7m2.labsis.frc.utn.edu.ar>
2023-08-24 19:05:46 -0300

    Codigo postal

---
+++ Archivos/CodigoPostal/cp.py
@@ -0,0 +1,48 @@
+def generar_lista(csv):
+    lista = []
+    for item in csv.readlines():
+        data = item.strip().split(";")
+        data[1] = int(data[1])
+        lista.append(data)
+    return lista
+
+def buscar_codigo(cod, lista):
+    for i, item in enumerate(lista):
+        if item[1] == cod:
+            return i
+    return -1
+
+def ingreso_codigos():
+    codigos = []
+    codigo = 1
+    while codigo != 0:
+        codigo = int(input("Ingrese codigo postal (0 para terminar): "))
+        if codigo != 0:
+            codigos.append(codigo)
+    return codigos
+
+def imprimir_datos(codigos, datos):
+    codigos_no_encontrados = []
+    print("\nCodigos encontrados:")
+    print("NUMERO|PROVINCIA|NOMBRE")
+    for codigo in codigos:
+        indice = buscar_codigo(codigo, datos)
+        if indice != -1:
+            print(f"{codigo:^6}|{datos[indice][0]:^9}|{datos[indice][2]:^5}")
+        else:
+            codigos_no_encontrados.append(codigo)
+    
+    if len(codigos_no_encontrados) > 0:
+        print("Los codigos no encontrados fueron:", codigos_no_encontrados)
+
+
+
+def main():
+    archivo = open("cp.csv", "r")
+    datos = generar_lista(archivo)
+    codigos = ingreso_codigos()
+    imprimir_datos(codigos, datos)
+
+
+if __name__ == "__main__":
+    main()
