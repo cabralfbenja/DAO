256cdeac153f61cb7610fc7c9e546b37a515fa87
Cabral Farré <cabralbenja2001@gmail.com>
2023-08-31 17:56:40 -0300

    Ejercicio Diccionario terminado

--- Diccionario/personas.py
+++ Diccionario/personas.py
@@ -18,9 +18,9 @@ def menu():
     print("Bienvenido... Elija la opcion que desee:")
     print("1) Busqueda por documento")
     print("2) Busqueda por apellido")
-    print("3) MOstrar promedios")
+    print("3) Mostrar promedios")
     print("4) Salir")
-    opcion = int(input("Opcion:"))
+    opcion = int(input("Opcion: "))
     return opcion
 
 def leer_archivo(f):
@@ -28,13 +28,49 @@ def leer_archivo(f):
     diccionario = dict()
     for linea in archivo.readlines():
         items = linea.strip().split(",")
-        diccionario[items[0]] = items[1:5]
+        diccionario[items[0]] = dict()
+        diccionario[items[0]]["nombre"] = items[1]
+        diccionario[items[0]]["apellido"] = items[2]
+        diccionario[items[0]]["edad"] = int(items[3])
     return diccionario
+
 
 
 def busqueda_por_dni(personas):
     dni = input("Ingrese un dni:")
-    print(personas[dni]) 
+    if dni in personas:
+        registro = personas[dni]
+        print("Registro encontrado:")
+        print("Nombre:", registro["nombre"])
+        print("Apellido:", registro["apellido"])
+        print("Edad:", registro["edad"])
+    else:
+        print("Registro no encontrado...")
+
+def buscar_apellido(diccionario, apellido_buscar):
+    resultados = []
+    for clave, registro in diccionario.items():
+        if registro["apellido"].lower() == apellido_buscar.lower():
+            resultados.append(registro)
+    return resultados
+
+def busqueda_por_apellido(personas):
+    apellido_a_buscar = input("Ingrese un apellido:")
+    apellidos = buscar_apellido(personas, apellido_a_buscar)
+    for registro in apellidos:
+        print("Nombre:", registro["nombre"])
+        print("Apellido:", registro["apellido"])
+        print("Edad:", registro["edad"])
+        print()
+    if len(apellidos) == 0:
+        print("Registro no encontrado...")
+
+def mostrar_promedio(personas):
+    suma = 0
+    for clave, persona in personas.items():
+        suma += persona["edad"]
+    print("El promedio de edades: ", round(suma/len(personas),2))
+
 
 def main():
     opcion = 0
@@ -42,7 +78,7 @@ def main():
     
     while opcion != 4:
         opcion = menu()
-        
+        print()
         if opcion == 1:
             busqueda_por_dni(personas)
         elif opcion == 2:
