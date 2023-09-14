a0b34dfeb64662ac80ec9c6f84db2db93b7a38b2
Cabral Farré <cabralbenja2001@gmail.com>
2023-09-14 10:38:30 -0300

    Personas 2

--- Objetos/Personas2/main.py
+++ Objetos/Personas2/main.py
@@ -21,18 +21,28 @@ def leer_archivo(f):
     archivo.close()
     return diccionario
 
+def cantidad_por_apellido(personas):
+    diccionario_apellidos = dict()
+    for p in personas.values():
+        if p.apellido in diccionario_apellidos:
+            diccionario_apellidos[p.apellido] += 1
+            continue
+        diccionario_apellidos[p.apellido] = 1
+    return diccionario_apellidos
+
 def main():
     personas = leer_archivo("personas.csv")
     personas_mayores_edad = len(list(filter(lambda persona: persona.esMayor(), personas.values())))
-    personas_con_vocal = list(filter(lambda p: p.nombreConVocal(), personas.values()))
-    personas_con_vocal = list(filter(lambda p: p.apellidoConVocal(), personas.values()))
+    apellidos_con_vocal= list(filter(lambda p: p.apellidoConVocal(), personas.values()))
+    personas_vocal = list(map(lambda p: f"{p.nombre} {p.apellido}", apellidos_con_vocal))
     print(f"Cantidad de personas: {len(personas)}")
     print(f"Cantidad de personas: {personas_mayores_edad}")
-    print("Listado de nombres con vocal:")
-    print(nombres_con_vocal)
-    print("Listado de apellidos con vocal:")
-    print(apellidos_con_vocal)
-    
+    print("Listado de nombres y apellidos de aquellas personas cuyo apellido empiece con vocal:")
+    for nombre_completo in personas_vocal:
+        print(nombre_completo)
+    diccionario_apellidos = cantidad_por_apellido(personas)
+    for persona, cantidad in diccionario_apellidos.items():
+        print(f"{persona}: {cantidad}")
 
 
 if __name__ == "__main__":
